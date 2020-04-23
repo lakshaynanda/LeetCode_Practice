b6f002ef28618692bb3b548756747a65d310318e
Lakshay Nanda <vineetnanda@Lakshays-MacBook-Air.local>
2020-04-23 16:44:11 +0530

    Update 3

---
+++ Q938BSTSum.java
@@ -0,0 +1,23 @@
+
+public class Q938BSTSum {
+    static int ans;
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        ans = 0;
+        dfs(root, L, R);
+        return ans;
+    }
+    public static void dfs(TreeNode node, int L, int R) {
+        if (node != null) {
+            if (L <= node.val && node.val <= R) {
+                ans += node.val;
+            }
+            if (L < node.val) {
+                dfs(node.left, L, R);
+            }
+            if (node.val < R) {
+                dfs(node.right, L, R);
+            }
+        }
+    }
+
+}
