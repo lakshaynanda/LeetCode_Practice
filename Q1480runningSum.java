f074e80234373d3b66d02b9bcb61f200d2fc3074
lakshaynanda <lakshay.nanda08@gmail.com>
2020-06-26 16:49:51 +0530

    running sum

---
+++ Q1480runningSum.java
@@ -0,0 +1,9 @@
+class runningSum {
+    public int[] runningSum(int[] nums) {
+        for(int i=1;i<nums.length;i++)
+        {
+            nums[i]+=nums[i-1];
+        }
+        return nums;
+    }
+}
